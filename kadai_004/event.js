bdb239b3e184e7df3554c65aa88cacb9f13abeeb
ayakonaka77 <161122549+ayakonaka77@users.noreply.github.com>
2024-04-18 19:06:13 +0900

    kadai_004を追加

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+//HTMLドキュメントが読み込み完了したとき
+$(window).on('load', function(){
+    console.log('loadイベントが発生しました');
+});
+
+// ボタンがクリックされた場合
+$(window).on('scroll', function(){
+    console.log('scrollイベントが発生しました');
+});
+ 
